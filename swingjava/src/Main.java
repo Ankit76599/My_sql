59bdad183fe6aac7dc7d9b9bd42fb11314af7340
Ankit <bca403ankit@gmail.com>
2024-11-28 22:24:48 +0530

    new

--- swingjava/src/Main.java
+++ swingjava/src/Main.java
@@ -1,9 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
 
 public class Main
 {
     public static void main(String[] args) {
-
 
         //        make a object of JFRAME
         JFrame fram=new JFrame();
@@ -15,12 +16,44 @@ public class Main
 
         fram.setVisible(true);//but if you want to close then it not terminate the program
 
-        fram.setBounds(100,100,1000,500);// setbound provide the height and width or position in the screen
+        fram.setBounds(0,0,1080,1080);// setbound provide the height and width or position in the screen
 
 
 //        close or program termination when click on close
         fram.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        System.out.println("hello java");
+
+//   set title in frame
+        fram.setTitle("My new project");
+
+
+        System.out.println("first");
+//     set icon in frame and mostly provide absolute path of image
+        ImageIcon im=new ImageIcon("p.png");
+        fram.setIconImage(im.getImage());
+
+        System.out.println("hello");
+
+//        background color and conatiner is in java.awt.*
+        Container c=fram.getContentPane();
+//        c.setBackground(Color.red);
+
+//        resizable window
+
+        fram.setResizable(true);  //default
+
+        System.out.println("hgvgsha");
+
+//        set text
+        c.setLayout(null);
+        JLabel jl=new JLabel();
+        jl.setBounds(100,50,100,30);
+        jl.setText("hello");
+        c.add(jl);
+        ImageIcon icon=new ImageIcon("p.png");
+        JLabel label=new JLabel(icon);
+        label.setBounds(100,50,icon.getIconWidth(),icon.getIconHeight());
+        System.out.println("end");
+
     }
 }
